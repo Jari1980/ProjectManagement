f0517d6bbd5bafb128f1104e9b18b0e52f3a525b
jari.tenhunen <jari.tenhunen@outlook.com>
2025-12-08 11:18:10 +0100

    Initial commit

---
+++ src/test/java/com/projectmanagemant/ProjectManagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectmanagemant.ProjectManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
