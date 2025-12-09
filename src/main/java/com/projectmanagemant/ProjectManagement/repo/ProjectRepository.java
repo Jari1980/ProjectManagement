e9132c64d7f70d060002a0651d822379761faffb
jari.tenhunen <jari.tenhunen@outlook.com>
2025-12-09 11:45:53 +0100

    repositories added, all extends JPARepository

---
+++ src/main/java/com/projectmanagemant/ProjectManagement/repo/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.projectmanagemant.ProjectManagement.repo;
+
+import com.projectmanagemant.ProjectManagement.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
